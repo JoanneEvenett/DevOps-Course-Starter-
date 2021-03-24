8a7394b680abc90acdb7eab8ac05ff0ba1d1b0dd
= <joanne.evenett@capita.co.uk>
2021-03-24 09:27:23 +0000

    Update todos and comments

--- todo_app/app.py
+++ todo_app/app.py
@@ -3,62 +3,33 @@ from todo_app.flask_config import Config
 from todo_app.model.trellomodel import *
 from todo_app.trelloApiCalls import *
 
-import requests
+import requests, logging
 
 app = Flask(__name__)
 app.config.from_object(Config)
+log = logging.getLogger(__name__)
 
 
 @app.route('/')
 def cards():
-    # get secrets
-    secrets = file_read("todo_app\secrets.txt")
-    boardId = secrets[0]
-    key =  secrets[1] 
-    token = secrets[2]
-
-    url = "https://api.trello.com/1/boards/{0}/lists" # root url could be stored and configured globally fpr re-use 
-
-    query = {
-        'key': key,
-        'token': token
-    }
-
-    response = requests.request(
-        "GET",
-        url.format(boardId),
-        params=query
-    )
 
     boardLists = []
-
-    results = response.json()
+    results = GetListsOnBoard()
 
     for index, boardListItem in enumerate(results): 
-        urlCards = "https://api.trello.com/1/boards/{0}/cards" # root url could be stored and configured globally fpr re-use 
-
-        queryCards = {
-            'key': key,
-            'token': token,
-            'idList': boardListItem["id"] ##doesnt seem to filter :-(
-        }
-
-        responseCards = requests.request(
-            "GET",
-            urlCards.format(boardId),
-            params=queryCards
-        )
-
+       
         cardsLists = []
+        responseCards = GetCardsonBoard(boardListItem["id"])
 
-        #assumes ordered by position
-        for cardItem in responseCards.json(): 
+        #default ordering by position (as per created sequence)
+        for cardItem in responseCards: 
             if cardItem["idList"] == boardListItem["id"]:
                 cardsLists.append(CardListItem(cardItem))
 
         idpreviousList = ""
         idnextList = ""
 
+        #crude, but retrieving the previous and next list, required for page to move
         if index == 0:
            idnextList = results[index+1]["id"]
         elif index == len(results) - 1:
@@ -67,105 +38,40 @@ def cards():
            idnextList = results[index+1]["id"]
            idpreviousList = results[index-1]["id"]
 
-        boardLists.append(BoardListClass(boardListItem, cardsLists, idpreviousList, idnextList ))
+        boardLists.append(BoardListClass(boardListItem, cardsLists, idpreviousList, idnextList))
     
     return render_template("cards.html", cards = boardLists)
 
-@app.route('/cards', methods=['POST']) # could be feasible to post entire card object back
+@app.route('/cards', methods=['POST'])
 def addCard():
-    #read form data - newItem - submitted in the POST request
     newCard = request.form['newCard']
-    secrets = file_read("todo_app\secrets.txt")
-    boardId = secrets[0]
-    key =  secrets[1] 
-    token = secrets[2]   
-    idList = secrets[3]   # first list
-
-    url = 'https://api.trello.com/1/lists/{0}/cards'
-
-    query = {
-        'key': key,
-        'token': token,
-        'name' : newCard
-    } 
-    
-    response = requests.request(
-        "POST",
-        url.format(idList),
-        params=query
-    )
+    AddCardtoFirstList(newCard)
 
     return redirect('/')
 
-@app.route('/cards/moveCardUp/<idCard>/<idNextList>') #Put
-def moveCardUp( idCard, idNextList):   
+@app.route('/cards/moveCardUp/<idCard>/<idNextList>') 
+def completeTask( idCard, idNextList):   
 
-    secrets = file_read("todo_app\secrets.txt")
-    boardId = secrets[0]
-    key =  secrets[1] 
-    token = secrets[2]   
-
-    url = 'https://api.trello.com/1/cards/{0}'
-
-    headers = {
-    "Accept": "application/json"
-    }
-
-    query = {
-        'key': key,
-        'token': token,
-        'idList' : idNextList
-    } 
-    
-    response = requests.request(
-        "PUT",
-        url.format(idCard),
-        headers=headers,
-        params=query,
-    )
+    MoveCard(idCard, idNextList)
 
     return redirect('/')
 
 
-@app.route('/cards/moveCardUp/<idCard>/<idPreviousList>') #Put
-def moveCardDown( idCard, idPreviousList):   
+@app.route('/cards/moveCardUp/<idCard>/<idPreviousList>') 
+def redoTask( idCard, idPreviousList):   
 
-    secrets = file_read("todo_app\secrets.txt")
-    boardId = secrets[0]
-    key =  secrets[1] 
-    token = secrets[2]   
-
-    url = 'https://api.trello.com/1/cards/{0}'
-
-    headers = {
-    "Accept": "application/json"
-    }
-
-    query = {
-        'key': key,
-        'token': token,
-        'idList' : idPreviousList
-    } 
-    
-    response = requests.request(
-        "PUT",
-        url.format(idCard),
-        headers=headers,
-        params=query,
-    )
+    MoveCard(idCard, idPreviousList)
 
     return redirect('/')
 
 
-def file_read(fname):
-        content_array = []
-        with open(fname) as f:
-                #Content_list is the list that contains the read lines.     
-                for line in f:
-                        content_array.append(line.rstrip())
-        
-        return content_array
+@app.route('/cards/<idCard>/<descriptionCard>/<duedate>') # TODO could be feasible to post entire card object back?
+def updateTask( idCard, descriptionCard, duedate):  
 
+    log.info(descriptionCard)
+    UpdateCard(idCard, descriptionCard, duedate)
+
+    return redirect('/')
 
 if __name__ == '__main__':
     app.run()
--- todo_app/trelloApiCalls.py
+++ todo_app/trelloApiCalls.py
@@ -1,1 +1,142 @@
 
+#TODO create global static variables such as Trello base url  
+
+import requests, logging
+
+log = logging.getLogger(__name__)
+
+def GetListsOnBoard():
+    secrets = file_read("todo_app\secrets.txt")
+    boardId = secrets[0]
+    key =  secrets[1] 
+    token = secrets[2]
+
+    log.info(boardId)
+    log.info(key)
+    log.info(token)
+
+    url = "https://api.trello.com/1/boards/{0}/lists" 
+
+    query = {
+        'key': key,
+        'token': token
+    }
+
+    response = requests.request(
+        "GET",
+        url.format(boardId),
+        params=query
+    )
+
+    return response.json()
+
+def GetCardsonBoard(idList):
+    secrets = file_read("todo_app\secrets.txt")
+    boardId = secrets[0]
+    key =  secrets[1] 
+    token = secrets[2]
+
+    log.info(boardId)
+    log.info(key)
+    log.info(token)#
+    urlCards = "https://api.trello.com/1/boards/{0}/cards" # root url could be stored and configured globally fpr re-use 
+
+    queryCards = {
+        'key': key,
+        'token': token,
+        'idList': idList ##doesnt seem to filter :-(
+    }
+
+    responseCards = requests.request(
+        "GET",
+        urlCards.format(boardId),
+        params=queryCards
+    )
+
+    return responseCards.json()
+
+def AddCardtoFirstList(newCard):
+    secrets = file_read("todo_app\secrets.txt")
+    boardId = secrets[0]
+    key =  secrets[1] 
+    token = secrets[2]   
+
+    listsonBoard = GetListsOnBoard()
+
+    idList = listsonBoard[0]["id"]
+
+    url = 'https://api.trello.com/1/lists/{0}/cards'
+
+    query = {
+        'key': key,
+        'token': token,
+        'name' : newCard
+    } 
+    
+    response = requests.request(
+        "POST",
+        url.format(idList),
+        params=query
+    )
+
+#TODO be good to change position so appear moved to bottom of list
+def MoveCard(idCard, idList):
+    secrets = file_read("todo_app\secrets.txt")
+    boardId = secrets[0]
+    key =  secrets[1] 
+    token = secrets[2]   
+
+    url = 'https://api.trello.com/1/cards/{0}'
+
+    headers = {
+    "Accept": "application/json"
+    }
+
+    query = {
+        'key': key,
+        'token': token,
+        'idList' : idList
+    } 
+    
+    response = requests.request(
+        "PUT",
+        url.format(idCard),
+        headers=headers,
+        params=query,
+    )
+
+
+#TODO be good to update entire object as per on page
+def UpdateCard(idCard, description, duedate):
+    secrets = file_read("todo_app\secrets.txt")
+    boardId = secrets[0]
+    key =  secrets[1] 
+    token = secrets[2]   
+
+    url = 'https://api.trello.com/1/cards/{0}'
+
+    headers = {
+    "Accept": "application/json"
+    }
+
+    query = {
+        'key': key,
+        'token': token,
+        'desc' : description,
+        'due' : duedate
+    } 
+    
+    response = requests.request(
+        "PUT",
+        url.format(idCard),
+        headers=headers,
+        params=query,
+    )
+def file_read(fname):
+        content_array = []
+        with open(fname) as f:
+                #Content_list is the list that contains the read lines.     
+                for line in f:
+                        content_array.append(line.rstrip())
+        
+        return content_array
